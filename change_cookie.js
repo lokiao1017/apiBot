9b18683b88c48db47f481095f478e88e974bea9d
lokiao1017 <kaisleyhan@gmail.com>
2025-05-09 23:12:45 +0800

    Update change_cookie.js

--- change_cookie.js
+++ change_cookie.js
@@ -1,4 +1,4 @@
-const get_cookies => () => {
+const get_cookies = () => {
     return {
         "datadome": "urFY95aGQTSGQmtKeaCd2Ly0eNlEINzj11XzHFdjxDmtnqUngZnAIxxIKPkBbdGWfHMU~YTqgxq39MVLtwEzXuKA_9za_av7abKqD73hrhAKoJpTqMEzkFbAr59rmCJj"
     };
