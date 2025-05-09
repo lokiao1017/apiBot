051dbde70e7d11c5c8b417db1a4d27b9f8bee7b6
lokiao1017 <kaisleyhan@gmail.com>
2025-05-09 23:03:19 +0800

    Update change_cookie.js

--- change_cookie.js
+++ change_cookie.js
@@ -1,4 +1,4 @@
-function getCookies() {
+function get_cookies() {
     return {
         "datadome": "urFY95aGQTSGQmtKeaCd2Ly0eNlEINzj11XzHFdjxDmtnqUngZnAIxxIKPkBbdGWfHMU~YTqgxq39MVLtwEzXuKA_9za_av7abKqD73hrhAKoJpTqMEzkFbAr59rmCJj"
     };
